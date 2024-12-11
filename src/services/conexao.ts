290617e9ab8f9db0bf6245c09bea46682087a46b
Carlos Eduardo-PC_trabalho <eduardo201184@hotmail.com>
2024-12-11 17:55:47 -0300

    feature: colocando o token em todas as rotas da requisição em todas rotas

--- src/services/conexao.ts
+++ src/services/conexao.ts
@@ -6,65 +6,85 @@ import axios from 'axios';
 
 const url = import.meta.env.VITE_BACKEND_URL;
 
+//configuração do axios
+const api = axios.create({
+  baseURL: url,
+});
+
+//colocando o token em todas as requisições de todas as rotas
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token'); // Obter o token do localStorage
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Adicionar o token no cabeçalho
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
 //login
 export const postLogin = async (newLogin: any) => {
-  const response = await axios.post(`${url}/auth/login`, newLogin);
+  const response = await api.post(`/auth/login`, newLogin);
   return response.data;
 };
 
 // Posts
 export const getPosts = async () => {
-  const response = await axios.get(`${url}/api/posts`);
+  const response = await api.get(`/api/posts`);
   return response.data;
 };
 
 export const createPosts = async (postNew: PostNew) => {
-  const response = await axios.post(`${url}/api/posts`, postNew);
+  const response = await api.post(`/api/posts`, postNew);
   return response.data;
 };
 
 // Usuarios
 export const getUser = async () => {
-  const response = await axios.get(`${url}/api/users`);
+  const response = await api.get(`/api/users`);
   return response.data;
 };
 export const getUserPorId = async (id: UsersInterface) => {
-  const response = await axios.get(`${url}/api/users/${id}`);
+  const response = await api.get(`/api/users/${id}`);
   return response.data;
 };
 
 export const createUser = async (user: UserSchema) => {
-  const response = await axios.post(`${url}/auth/register`, user);
+  const response = await api.post(`/auth/register`, user);
   return response.data;
 };
 
 // Perfil
 export const getPerfilPorId = async (id: number) => {
-  const response = await axios.get(`${url}/api/profile/${id}`);
+  const response = await api.get(`/api/profile/${id}`);
   return response.data;
 };
 
 // Comentarios
 export const getComentarios = async () => {
-  const response = await axios.get(`${url}/api/comments`);
+  const response = await api.get(`/api/comments`);
   return response.data;
 };
 
 export const getComentariosPorIdDoPost = async (id: any) => {
-  const response = await axios.get(`${url}/api/comments/post/${id}`);
+  const response = await api.get(`/api/comments/post/${id}`);
   return response.data;
 };
 export const CreateComentarioNoPost = async (newComentario: NewComentario) => {
-  const response = await axios.post(`${url}/api/comments`, newComentario);
+  const response = await api.post(`/api/comments`, newComentario);
   return response.data;
 };
 
 //deletar
 export const apagarPost = async (id: any) => {
-  const response = await axios.delete(`${url}/api/posts/${id}`);
+  const response = await api.delete(`/api/posts/${id}`);
   return response.data;
 };
 export const apagarComentario = async (id: any) => {
-  const response = await axios.delete(`${url}/api/comments/${id}`);
+  const response = await api.delete(`/api/comments/${id}`);
   return response.data;
 };
